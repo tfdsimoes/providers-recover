1bf54b87549860ae0f5bc3fa9c459f35e0ae2697
tfdsimoes <tfdsimoes@gmail.com>
2021-02-02 17:04:35 +0100

    Add entity providers

--- src/main/java/es/duonet/providers/entity/Providers.java
+++ src/main/java/es/duonet/providers/entity/Providers.java
@@ -1,2 +1,55 @@
-package es.duonet.providers.entity;public class Providers {
+package es.duonet.providers.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Providers {
+
+    @Id
+    @Column ( name = "id")
+    private int id;
+
+    @Column ( name = "name")
+    private String name;
+
+    @Column ( name = "inserted")
+    private LocalDateTime inserted;
+
+    @Column ( name = "id_client")
+    private int idClient;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getInserted() {
+        return inserted;
+    }
+
+    public void setInserted(LocalDateTime inserted) {
+        this.inserted = inserted;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
 }
