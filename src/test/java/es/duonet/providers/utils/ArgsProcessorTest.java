941de891cc7d7f6f10af8b9684408fd97c465562
tfdsimoes <tfdsimoes@gmail.com>
2021-02-02 19:54:59 +0100

    Add tests

---
+++ src/test/java/es/duonet/providers/utils/ArgsProcessorTest.java
@@ -0,0 +1,11 @@
+package es.duonet.providers.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ArgsProcessorTest {
+
+    @Test
+    void getIdClient() {
+    }
+}
