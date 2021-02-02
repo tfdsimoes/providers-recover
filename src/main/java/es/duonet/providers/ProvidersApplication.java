5c33db28ec9af9cf87741a7c6e8ec4c1061fae46
tfdsimoes <tfdsimoes@gmail.com>
2021-02-02 16:41:01 +0100

    Start of java project

---
+++ src/main/java/es/duonet/providers/ProvidersApplication.java
@@ -0,0 +1,18 @@
+package es.duonet.providers;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvidersApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvidersApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+
+	}
+}
---
+++ src/main/java/es/duonet/providers/ValidatorArgs.java
@@ -0,0 +1,4 @@
+package es.duonet.providers;
+
+public class ValidatorArgs {
+}
